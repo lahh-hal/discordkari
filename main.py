4519f1096087da7cbd2193d7785a2c2bbfa2808e
hal <114328720+lahh-hal@users.noreply.github.com>
2023-05-23 17:57:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 
-TOKEN='MTExMDE1OTQwODEwNjg0MDE4Nw.GcF6Xe.E3V6464aFLoZ4zK3AUT9bcPACpecFUAugKcy6g'
+TOKEN='MTExMDE1OTQwODEwNjg0MDE4Nw.Ghisox.6WDidCFeXhNvpyZe7Yo7sBN7LY0uIWaygN9uvI'
 intents=discord.Intents.default()
 intents.message_content=True
 client =discord.Client(intents=intents)
